cf2604efa9eeb20da9e897bb55feaa797423a1cf
samuelweekes <samueljweekes@gmail.com>
2019-08-14 23:26:42 +0100

    update for heroku

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/study', function(req, res){
   });
 });
 
-const hardCodedId = '5d4f1b7fd465d35adc4e762b';
+const hardCodedId = '5d548a1399511da77268b222';
 app.get('/account', function(req, res){
   Mongo.User.findOne({_id : hardCodedId}, (err, account) => {
     if(err){
