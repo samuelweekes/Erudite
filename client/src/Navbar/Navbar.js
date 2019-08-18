7ca3ffaf5f003c7668cb9e80f308707d99ee4707
samuelweekes <samueljweekes@gmail.com>
2019-08-18 18:45:52 +0100

    Added mobile menu

--- client/src/Navbar/Navbar.js
+++ client/src/Navbar/Navbar.js
@@ -7,6 +7,20 @@ import stats    from '../assets/stats.svg';
 import './Navbar.css';
 
 class Navbar extends React.Component {
+  constructor(props){
+    super(props);
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.state = {mobileMenu: false};
+  }
+
+  toggleMenu(){
+    this.setState({mobileMenu : !this.state.mobileMenu});
+    if(this.state.mobileMenu === true){
+      let checkbox = document.getElementById("navCheck");
+      checkbox.checked = false;
+    }
+  }
+
   render() {  
     let img;
     switch(this.props.location.pathname){
@@ -27,45 +41,55 @@ class Navbar extends React.Component {
     }
 
     return (
-      <nav className="navBar">
-        <div className="outerContainer">
-          <div>
-            <div className="imgOuter">
-              <Link to="/" className="">
-                <img src={img} className="img" />
-              </Link>
+      <div>
+        <nav onClick={this.toggleMenu} className="mobile-nav" role="navigation">
+          <div className="menuToggle">
+            <input id="navCheck" type="checkbox" />
+            <span className="mobile-span"></span>
+            <span className="mobile-span"></span>
+            <span className="mobile-span"></span>
+          </div>
+        </nav>
+        <nav className={`navBar${this.state.mobileMenu ? ' fullScreenMenu' : ''}`} >
+          <div className="outerContainer">
+            <div>
+              <div className="imgOuter">
+                <Link onClick={this.toggleMenu} to="/" className="">
+                  <img src={img} className="img" />
+                </Link>
+              </div>
+              <div className="imgSeperator"> 
+                <hr className="imgLine"/>
+              </div>
             </div>
-            <div className="imgSeperator"> 
-              <hr className="imgLine"/>
+            <div className="linkContainer">
+              <ul>
+                <li>
+                  <Link className="" onClick={this.toggleMenu} to="/">Home</Link>
+                </li>
+                <div className="seperator"> 
+                  <hr className="line"/>
+                </div>
+                <li>
+                  <Link className="" onClick={this.toggleMenu} to="/account">Account</Link>
+                </li>
+                <div className="seperator"> 
+                  <hr className="line"/>
+                </div>
+                <li>
+                  <Link className="" onClick={this.toggleMenu} to="/sessions">Sessions</Link>
+                </li>
+                <div className="seperator"> 
+                  <hr className="line"/>
+                </div>
+                <li>
+                  <Link className="" onClick={this.toggleMenu} to="/stats">Stats</Link>
+                </li>
+              </ul>
             </div>
           </div>
-          <div className="linkContainer">
-            <ul>
-              <li>
-                <Link className="" to="/">Home</Link>
-              </li>
-              <div className="seperator"> 
-                <hr className="line"/>
-              </div>
-              <li>
-                <Link className="" to="/account">Account</Link>
-              </li>
-              <div className="seperator"> 
-                <hr className="line"/>
-              </div>
-              <li>
-                <Link className="" to="/sessions">Sessions</Link>
-              </li>
-              <div className="seperator"> 
-                <hr className="line"/>
-              </div>
-              <li>
-                <Link className="" to="/stats">Stats</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     );
   }
 }
