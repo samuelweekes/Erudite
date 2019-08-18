c1be5923878b145aab169522ceebe2233b60c5b5
samuelweekes <samueljweekes@gmail.com>
2019-08-18 21:05:00 +0100

    Bugfix

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const Mongo = require('./models/index');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
